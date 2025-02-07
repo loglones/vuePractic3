762038f93273e62b97acb8f48f8536fdd7cbc210
DESKTOP-QL9B2DK\DimASSik <goose9098@gmail.com>
2025-02-07 12:50:12 +0300

    добавил вторую колонку,обновил методы удаления редактирования и перемещения,добавил массив для второй колонки,теперь методам из kanban-column так же передается title в качестве параметра ,в эмитированные функции так же передается title, изменил html использование компонентов,в эмитированных функциях стал передавать стрелочную функцию

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ Vue.component('kanban-column', {
     template: `
     <div class="kanban-columns">
         <h2>{{ title }}</h2>
-        <div class="task-form" v-if="title === 'Запланированные задачи'">  \\ ?
+        <div class="task-form" v-if="title === 'Запланированные задачи'">
             <input type="text" placeholder="Заголовок задачи" v-model="newTask.title" @keyup.enter="submitTask" />
             <textarea placeholder="Описание задачи" v-model="newTask.description" @keyup.enter="submitTask"></textarea>
-            <input type="date" placeholder="Дэдлайн" v-model="newTask.deadline"@keyup.enter="submitTask" />
+            <input type="date" placeholder="Дэдлайн" v-model="newTask.deadline" @keyup.enter="submitTask" />
             <button @click="submitTask">Добавить задачу</button>
         </div>
         <div v-if="tasks.length > 0">
@@ -14,9 +14,9 @@ Vue.component('kanban-column', {
                     v-for="(task, index) in tasks" 
                     :key="task.id" 
                     :task="task" 
-                    @delete="handleDelete(index)" 
-                    @edit="handleEdit(task, index)" 
-                    @move="handleMove(index)">
+                    @delete="$emit('delete-task', title, index)" 
+                    @edit="$emit('edit-task', title, task, index)" 
+                    @move="$emit('move-task', title, index)">
              </task-card>
         </div>
         <p v-else>Задач нет</p>
@@ -32,8 +32,7 @@ Vue.component('kanban-column', {
     },
     methods: {
         submitTask() {
-            if(!this.newTask.title || !this.newTask.description || !this.newTask.deadline) return;
-
+            if (!this.newTask.title || !this.newTask.description || !this.newTask.deadline) return;
             const task = {
                 id: Date.now(),
                 createdAt: new Date().toLocaleString(),
@@ -42,21 +41,20 @@ Vue.component('kanban-column', {
             };
             console.log('Добавлена новая задача:', task);
             this.$emit('add-task', task);
-            this.newTask = {title: '', description: '', deadline: ''};
+            this.newTask = { title: '', description: '', deadline: '' };
         },
-        handleDelete(index) {
-            this.$emit('delete-task', index);
+        handleDelete(title, index) {
+            this.$emit('delete-task', title, index);
         },
-        handleEdit(task, index) {
+        handleEdit(title, task, index) {
             const editTask = { ...task, lastEdited: new Date().toLocaleString(), isEditing: true };
-            this.$emit('edit-task', editTask, index);
+            this.$emit('edit-task', title, editTask, index);
         },
-        handleMove(index) {
-            this.$emit('move-task', index);
+        handleMove(title, index) {
+            this.$emit('move-task', title, index);
         }
-
     }
-})
+});
 
 Vue.component('task-card', {
     props: ['task'],
@@ -100,20 +98,42 @@ new Vue({
     el: '#app',
     data: {
         plannedTasks: [],
+        inProgressTasks: [],
+
     },
     methods: {
         addTask(task) {
             this.plannedTasks.push(task);
         },
-        deleteTask(index) {
-            this.plannedTasks.splice(index, 1);
+        deleteTask(column, index) {
+            if (column === 'Запланированные задачи') {
+                this.plannedTasks.splice(index, 1);
+            } else if (column === 'Задачи в работе') {
+                this.inProgressTasks.splice(index, 1);
+            }
         },
-        editTask(updatedTask,index) {
-            this.$set(this.plannedTasks, index, updatedTask);
+        editTask(column, updatedTask, index) {
+            if (column === 'Запланированные задачи') {
+                this.$set(this.plannedTasks, index, updatedTask);
+            } else if (column === 'Задачи в работе') {
+                this.$set(this.inProgressTasks, index, updatedTask);
+            }
         },
-        moveTask(index) {
-            const movedTask = this.plannedTasks.splice(index, 1)[0];
+        moveTask(fromColumn, toColumn, index) {
+            let movedTask;
 
+            if (fromColumn === 'Запланированные задачи') {
+                movedTask = this.plannedTasks.splice(index, 1)[0];
+                if (toColumn === 'Задачи в работе') {
+                    this.inProgressTasks.push(movedTask);
+                    console.log('Перемещено в "Задачи в работе":', movedTask);
+                }
+            } else if (fromColumn === 'Задачи в работе') {
+                movedTask = this.inProgressTasks.splice(index, 1)[0];
+                if (toColumn === 'Тестирование') {
+                    console.log('Перемещено в "Тестирование":', movedTask);
+                }
+            }
         }
     }
 })
